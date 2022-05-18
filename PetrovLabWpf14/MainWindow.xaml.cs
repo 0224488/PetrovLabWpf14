f43ed850ab9e9ff5d1e3c56bb30985f128f872df
0224488 <0224488@inbox.ru>
2022-05-18 17:45:41 +0300

    Добавьте файлы проекта.

---
+++ PetrovLabWpf14/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace PetrovLabWpf14
+{
+	// Определить класс Product для хранения информации о товаре.
+	// Класс должен содержать характеристики Название товара (строка), Цена (число), 
+	// Изображение (строка - путь к файлу с изображением), Категория(перечисление, возможные значения – Еда, Бытовая техника). 
+	// Разработать шаблон данных для отображения списка товаров в компоненте ListBox.
+	// При помощи триггеров определите разный шаблон для товаров категории Еда и категории Бытовая техника.
+
+	public partial class MainWindow : Window
+	{
+		private ObservableCollection<Product> products;
+
+		public Product Product { get; set; } = new Product();
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			products = new ObservableCollection<Product>();
+
+			DataContext = Product;
+			var categories = new Dictionary<CategoryTypes, string>();
+			categories.Add(CategoryTypes.Food, "еда");
+			categories.Add(CategoryTypes.Technics, "бытовая техника");
+			categoryComboBox.ItemsSource = categories;
+
+			productsList.ItemsSource = products;
+		}
+
+		// обработка кнопки выбора изображения
+		private void buttonSelectImage_Click(object sender, RoutedEventArgs e)
+		{
+			var dlg = new OpenFileDialog();
+			dlg.Filter = "jpg files (*.jpg)|*.jpg|Все файлы (*.*)|*.*";
+			if(dlg.ShowDialog() == true)
+			{
+				Product.ImagePath = dlg.FileName;
+			}
+		}
+
+		// обработка кнопки добавления продукта
+		private void buttonAddProduct_Click(object sender, RoutedEventArgs e)
+		{
+			products.Add(new Product()
+			{
+				Name = Product.Name,
+				Price = Product.Price,
+				ImagePath = Product.ImagePath,
+				Category = Product.Category,
+			});
+			// сброс к значению по-умолчанию
+			Product.Reset();
+		}
+	}
+}
---
+++ PetrovLabWpf14/Product.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace PetrovLabWpf14
+{
+	public enum CategoryTypes
+	{
+		Food = 0,
+		Technics = 1
+	}
+
+	public class Product : INotifyPropertyChanged
+	{
+		private string _name;
+		public string Name 
+		{
+			get { return _name; }
+			set { _name = value; OnPropertyChanged(); }
+		}
+
+		private double _price;
+		public double Price 
+		{
+			get { return _price; }
+			set { _price = value; OnPropertyChanged(); }
+		}
+		
+		private string _imagePath;
+		public string ImagePath 
+		{
+			get { return _imagePath; }
+			set { _imagePath = value; OnPropertyChanged(); }
+		}
+
+		private CategoryTypes _category;
+		public CategoryTypes Category 
+		{
+			get { return _category; }
+			set { _category = value; OnPropertyChanged(); }
+		}
+
+		public string CategoryName
+		{
+			get
+			{
+				if(Category == CategoryTypes.Food)
+					return "Еда";
+				else
+					return "Бытовая техника";
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		private void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+		}
+
+		/// <summary>
+		/// сброс к значению по-умолчанию
+		/// </summary>
+		public void Reset()
+		{
+			ImagePath = string.Empty;
+			Price = 0;
+			Name = string.Empty;
+			Category = CategoryTypes.Food;
+		}
+	}
+}
